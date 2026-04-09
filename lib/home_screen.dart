6a2248057dba8c0055ab4370dc0f9368a556c3fa
StillBlack <stillblack2014@gmail.com>
2026-04-09 05:17:37 +0500

    model is created

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,6 @@
+import 'package:api_world/models/post_model.dart';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -6,6 +8,17 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  
+  List<PostModel> PostList = [];
+  Future<List<PostModel>> getPostApi() async {
+    final response = await http.get(
+      Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+    );
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+    } else {}
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
