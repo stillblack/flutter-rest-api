c89baf4b64a598075d698bfe8e15ea5cc8502d80
StillBlack <stillblack2014@gmail.com>
2026-04-13 04:01:22 +0500

    updated cutom model

--- lib/example_two.dart
+++ lib/example_two.dart
@@ -12,6 +12,7 @@ class ExampleTwo extends StatefulWidget {
 
 class _ExampleTwoState extends State<ExampleTwo> {
   List<Photos> photosList = [];
+
   Future<List<Photos>> getPhotos() async {
     final response = await http.get(
       Uri.parse('https://jsonplaceholder.typicode.com/photos'),
@@ -19,9 +20,10 @@ class _ExampleTwoState extends State<ExampleTwo> {
     var data = jsonDecode(response.body.toString());
     if (response.statusCode == 200) {
       for (Map i in data) {
-        Photos photos = Photos(title: i['title'], url: i['url']);
+        Photos photos = Photos(title: i['title'], url: i['url'], id: i['id']);
         photosList.add(photos);
       }
+      return photosList;
     } else {
       return photosList;
     }
@@ -31,15 +33,37 @@ class _ExampleTwoState extends State<ExampleTwo> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(centerTitle: true, title: Text('Api World')),
-      body: Column(children:[
-        
-      ]),
+      body: Column(
+        children: [
+          Expanded(
+            child: FutureBuilder(
+              future: getPhotos(),
+              builder: (context, snapshot) {
+                return ListView.builder(
+                  itemBuilder: (context, index) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          snapshot.data![index].url.toString(),
+                        ),
+                      ),
+                      title: Text(snapshot.data![index].id.toString()),
+                      subtitle: Text(snapshot.data![index].title.toString()),
+                    );
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
 
 class Photos {
   String title, url;
+  int id;
 
-  Photos({required this.title, required this.url});
+  Photos({required this.title, required this.url, required this.id});
 }
