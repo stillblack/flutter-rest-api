1f60fdfe4654badd2ebd899787a9485a34acadd1
StillBlack <stillblack2014@gmail.com>
2026-04-24 18:28:52 +0500

    singup page is updated

--- lib/signup.dart
+++ lib/signup.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 import 'package:flutter/material.dart';
+import 'package:http/http.dart';
 
 class Signup extends StatefulWidget {
   const Signup({super.key});
@@ -12,6 +13,22 @@ class Signup extends StatefulWidget {
 class _SignupState extends State<Signup> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+
+  void Login(String email, String password) {
+    try{
+      Response response = await post(Uri.parse('url'),body:{'email':email , 'password':password});
+      if(response.statusCode == 200){
+        print('account created successfully');
+      }else{
+        print('failed');
+      }
+    }
+    catch(e){
+      print(e.toString());
+
+    }
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,8 +48,21 @@ class _SignupState extends State<Signup> {
               controller: passwordController,
               decoration: InputDecoration(hintText: 'password'),
             ),
-            SizedBox(height: 15),
-            
+            // SizedBox(height: 15),
+            SizedBox(height: 25),
+            TextButton(
+              onPressed: () {
+                Login(
+                  emailController.text.toString(),
+                  passwordController.text.toString(),
+                );
+              },
+              style: TextButton.styleFrom(
+                minimumSize: Size(double.infinity, 50),
+                backgroundColor: Colors.green,
+              ),
+              child: Text('Sign Up'),
+            ),
           ],
         ),
       ),
