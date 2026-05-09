0fffbc5cf615ac2205ce7d91da5d0abc470bb934
StillBlack <stillblack2014@gmail.com>
2026-05-10 04:24:28 +0500

    upload file / image through to  Rest API initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:api_world/example_five.dart';
-import 'package:api_world/signup.dart';
+// import 'package:api_world/example_five.dart';
+// import 'package:api_world/signup.dart';
+import 'package:api_world/upload_image.dart';
 // import 'package:api_world/home_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Api World',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: const Signup(),
+      home: const UploadImage(),
     );
   }
 }
---
+++ lib/upload_image.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class UploadImage extends StatefulWidget {
+  const UploadImage({super.key});
+
+  @override
+  State<UploadImage> createState() => _UploadImageState();
+}
+
+class _UploadImageState extends State<UploadImage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Upload Image'),
+        backgroundColor: Colors.lightBlue,
+      ),
+    );
+  }
+}
